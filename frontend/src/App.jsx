a4bdd48c011c7ade4e02671a3d71d17efe4b52b3
Paras Ningune <paras.ningune01@gmail.com>
2026-02-06 21:53:31 +0530

    working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import './styles/App.css'
 
 function App({ config = {} }) {
   const defaultConfig = {
-    apiUrl: 'https://amplifyease-task.onrender.com',
+    apiUrl: 'http://localhost:5001', //'https://amplifyease-task.onrender.com'
     primaryColor: '#667eea',
     position: 'bottom-right',
     title: 'Chat with us!',
